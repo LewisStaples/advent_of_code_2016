e3893cc64bee03f767b4d0f875c7db16870431fb
Lewis Staples <lewis.staples@gmail.com>
2022-09-02 22:29:02 -0400

    Day 04 part B working

--- day04/day04.py
+++ day04/day04.py
@@ -4,10 +4,21 @@
 
 from collections import Counter
 
+def decrypt_room_name(encrypted_room_name, sectorID):
+    encrypted_room_name_dashes2spaces = encrypted_room_name.replace('-', ' ')
+    decrypted_room_name_list = []
+    for ch in encrypted_room_name_dashes2spaces:
+        if ch.islower():
+            new_char_num = (ord(ch) + sectorID - ord('a')) % (ord('z') - ord('a') + 1) + ord('a')
+            decrypted_room_name_list.append(chr(new_char_num))
+        else:
+            decrypted_room_name_list.append(ch)
+    return ''.join(decrypted_room_name_list)
+
 sum_sectorIDs_realRooms = 0
 
 # Reading input from the input file
-input_filename='input_sample0.txt'
+input_filename='input_sample1.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     # Pull in each line from the input file
@@ -39,8 +50,13 @@ with open(input_filename) as f:
         if fail_flag == True:
             continue
 
-        sum_sectorIDs_realRooms += int(sectorID_list)
+        sectorID_int = int(sectorID_list)
+        sum_sectorIDs_realRooms += sectorID_int
+        decrypted_line = decrypt_room_name(line_except_checksum, sectorID_int)
+        # accomodate sample scenario or the true (graded) scenario
+        if ('encrypted' in decrypted_line) or ('north' in decrypted_line):
+            decrypted_room_name = decrypted_line
 
 print(f'The answer to part A is {sum_sectorIDs_realRooms}\n')
-
+print(f'The answer to part B is at the end of the next line \n{decrypted_room_name}\n')
 
