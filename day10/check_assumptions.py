f0dd71fe6f8a08c755a54d1075597e0153b62ba1
Lewis Staples <lewis.staples@gmail.com>
2022-09-11 18:16:31 -0400

    Day 10 Started over ... part A and B now working

--- day10/check_assumptions.py
+++ day10/check_assumptions.py
@@ -12,6 +12,10 @@
 set_bots_start = set()
 set_bots_later = set()
 
+set_values = set()
+
+bot_numChips_dict = dict()
+
 # Reading input from the input file
 input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}\n')
@@ -28,6 +32,26 @@ with open(input_filename) as f:
                 set_bots_later.add(int(in_string[i+1]))
         if 'bot' in in_string[2:]:
             dummy = 123
+
+        
+        # Verifying two other assumptions:
+        if in_string[0] == 'value':
+
+             # Check if values are unique integers
+            new_value = int(in_string[1])
+            if new_value in set_values:
+                raise ValueError('Error ... There is a repeated value!')
+            else:
+                set_values.add(new_value)
+
+            # Check if the initial placement of values doesn't lead to any bot with three or more chips
+            init_bot_num = int(in_string[5])
+            if init_bot_num not in bot_numChips_dict:
+                bot_numChips_dict[init_bot_num] = 1
+            elif bot_numChips_dict[init_bot_num] == 1:
+                bot_numChips_dict[init_bot_num] = 2
+            else:
+                ValueError(f'bot # {init_bot_num} has more than two chips at the start!')
 
 # Check if both sets are identical.  Since True is printed, both sets are identical.
 print(set_bots_start == set_bots_later)
--- day10/day10.py
+++ day10/day10.py
@@ -1,70 +1,26 @@
 # adventOfCode 2016 day 10
 # https://adventofcode.com/2016/day/10
 
-# from dataclasses import dataclass
-from enum import Enum
-from dataclasses import dataclass
-from abc import ABC
-import sys
 
+from enum import Enum
 
 class DestinationType(Enum):
     bot = 0,
     output = 1
 
-class Destination(ABC):
-    pass
 
-@dataclass
-class Output(Destination):
-    def __init__(self):
-        self.values = set()
-
-
-@dataclass
-class Bot(Destination):
+class Bot:
     def __init__(self, rule_input_list):
-        self.values = set()
+        self._values = set()
         self.bot_number = int(rule_input_list[1])
-        # print(rule_input_list)
 
-        # low rule
+        # create low rule
         i_low = rule_input_list.index('low')
-        self.low = {'dest_number': int(rule_input_list[i_low+3]), 'dest_type': DestinationType[rule_input_list[i_low+2]]}
+        self.low_rule = {'dest_number': int(rule_input_list[i_low+3]), 'dest_type': DestinationType[rule_input_list[i_low+2]]}
 
-        # high rule
+        # create high rule
         i_high = rule_input_list.index('high')
-        self.high = {'dest_number': int(rule_input_list[i_high+3]), 'dest_type': DestinationType[rule_input_list[i_high+2]]}
-
-        # Rule out edge case
-        if self.low['dest_number'] == self.high['dest_number']:
-            if self.low['dest_type'] == self.high['dest_type'] == DestinationType.bot:
-                ValueError('Edge case:  we cannot have a bot sending both chips to the same [other] bot')
-
-
-
-    def add_value(self, value):
-        self.values.add(value)
-        if 17 in self.values and 61 in self.values:
-            print(f'The answer to part A is {self.bot_number}')
-            sys.exit('Program Complete')
-    
-    def get_number_of_bots(self):
-        return len(self.values)
-
-    def remove_bots(self):
-        if len(self.values) < 2:
-            raise ValueError('not enough robots!')
-        low_val, high_val = min(self.values), max(self.values)
-        self.values = set()
-        return {'low': (low_val, self.low['dest_type'], self.low['dest_number']), 
-        'high': (high_val, self.high['dest_type'], self.high['dest_number'])}
-        
-
-@dataclass
-class OutputBin:
-    def __init__(self):
-        self.values = set()
+        self.high_rule = {'dest_number': int(rule_input_list[i_high+3]), 'dest_type': DestinationType[rule_input_list[i_high+2]]}
 
 
 # (not referring to facility as a "factory," less someone thinks I am referring to the factory design pattern)
@@ -83,94 +39,91 @@ class MicrochipFacility:
                 in_string = in_string.rstrip()
                 self.process_input_line(in_string)
 
-    
+        # Now, traverse self.initial_chip_locations
+        for value_num, bot_num in self.initial_chip_locations:
+            self.bots[bot_num]._values.add(value_num)
+
+    def add_value(self, dest_num, dest_type, value_num):
+        if dest_type == DestinationType.output:
+            if dest_num not in self.outputs:
+                self.outputs[dest_num] = [value_num]
+                return
+
+        assert len(self.bots[dest_num]._values) < 2
+        self.bots[dest_num]._values.add(value_num)
+
+
+    def transfer_chips(self, bot_num):
+        assert len(self.bots[bot_num]._values) <= 2
+
+        # Verify that the starting bot has two chips
+        if len(self.bots[bot_num]._values) < 2:
+            return 0
+
+        # Look for part A answer:
+        if self.bots[bot_num]._values == {17, 61}:
+            print(f'The answer to part A is bot # {bot_num}\n')
+
+
+        low_dest_num = self.bots[bot_num].low_rule['dest_number']
+        high_dest_num = self.bots[bot_num].high_rule['dest_number']
+        low_value = min(self.bots[bot_num]._values)
+        high_value = max(self.bots[bot_num]._values)
+
+        # If either destination is a bot with two chips, transfer that first
+        if self.bots[bot_num].low_rule['dest_type'] == DestinationType.bot:
+            if len(self.bots[low_dest_num]._values) == 2:
+                self.transfer_chips(self, low_dest_num)
+            self.bots[low_dest_num]._values.add(low_value)
+        if self.bots[bot_num].high_rule['dest_type'] == DestinationType.bot:
+            if len(self.bots[high_dest_num]._values) == 2:
+                self.transfer_chips(self, high_dest_num)
+            self.bots[high_dest_num]._values.add(high_value)
+
+        if self.bots[bot_num].low_rule['dest_type'] == DestinationType.output:
+            if low_dest_num not in self.outputs:
+                self.outputs[low_dest_num] = set()
+            self.outputs[low_dest_num].add(low_value)
+        if self.bots[bot_num].high_rule['dest_type'] == DestinationType.output:
+            if high_dest_num not in self.outputs:
+                self.outputs[high_dest_num] = set()
+            self.outputs[high_dest_num].add(high_value)
+
+        self.bots[bot_num]._values.clear()
+        return 1
+
     def process_input_line(self, in_string):
-        # print(in_string)
         in_string_list = in_string.split(' ')
 
-        # Transfer from "input" bin to a bot
+        #  Put record in self.initial_chip_locations
         if in_string_list[0] == 'value':
-            # assert in_string_list[4] == 'bot'
+            assert in_string_list[4] == 'bot'
             self.initial_chip_locations.append(
                 (int(in_string_list[1]), 
-                DestinationType[(in_string_list[4])], 
                 int(in_string_list[5]))
             )
-            
-            # print(in_string)
 
         # Record the rules for what to do when two items are in this bot
         elif in_string_list[0] == 'bot':
             self.bots[int(in_string_list[1])] = Bot(in_string_list)
         else:
+            # Input lines must start either with "value" or "bot"!
             raise ValueError('Bad input line: ' + in_string)
 
-    def start_transfers(self):
-        # Place chips in initial bot locations
-        for bot_loc in self.initial_chip_locations:
-            self.transfer_value(None, bot_loc)
-
-
+    def run_facility(self):
         while True:
+            ret_val = 0
             for bot_num in self.bots:
-                if len(self.bots[bot_num].values) == 2:
-                    # Verify that there is space to transfer the bots to
-                    if self.bots[bot_num].low['dest_type'] == DestinationType.bot:
-                        if len(self.bots[
-                            self.bots[bot_num].low['dest_number']
-                        ].values) >= 2:
-                            continue
-                    if self.bots[bot_num].high['dest_type'] == DestinationType.bot:
-                        if len(self.bots[
-                            self.bots[bot_num].high['dest_number']
-                        ].values) >= 2:
-                            continue
-
-                #     # Transfer the bots: low and then high
-                #     self.transfer_value((min(self.bots.values), DestinationType.bot, self.bots[bot_num].low['dest_number']), 
-                #     (self.bots.values[0], DestinationType.bot, self.bots[bot_num].high['dest_number']))
-                #     break
-
-                # elif len(self.bot[bot_num]) > 2:
-                #     raise ValueError(f'Bot # {bot_num} has more than two chips!!!')
-
-            # This should only happen if no record triggered a break from the for loop
-            break
-
-    def transfer_value(self, old_chiploc, new_chiploc):
-        if old_chiploc is not None:
-            old_chip_value = old_chiploc[0]
-            old_dest_num = old_chiploc[2]
-
-            # You can't transfer chips out of any output !!!
-            if old_chiploc[1] != DestinationType.bot:
-                raise ValueError('Bad Old LocationType: ' + old_chiploc[1])
-
-
-
-
-            self.bots[old_dest_num].values.remove(old_chip_value)
-
-        new_chip_value = new_chiploc[0]
-        new_dest_num = new_chiploc[2]
-
-        if new_chiploc[1] == DestinationType.bot:
-            self.bots[new_dest_num].add_value(new_chip_value)
-        else:
-            if new_dest_num in self.outputs:
-                self.outputs[new_dest_num].append(new_chip_value)
-            else:
-                self.outputs[new_dest_num] = [new_chip_value]
-
-
-    # def transfer_value_to(self):
-    #     pass
-
-    # def transfer_value_from(self):
-    #     pass
+                ret_val += self.transfer_chips(bot_num)
+            if ret_val == 0:
+                # Note that part A is not expected to get this far
+                print('All chips have been transferred now')
+                partB_soln = self.outputs[0].pop() * self.outputs[1].pop() * self.outputs[2].pop()
+                print(f'The solution to part B is {partB_soln}\n')
+                return
 
 microchipfacility = MicrochipFacility()
-microchipfacility.start_transfers()
+microchipfacility.run_facility()
 
-dummy = 123
+
 
