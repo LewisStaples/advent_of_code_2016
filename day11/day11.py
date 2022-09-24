5aa85c044996ee2ab199b10200c5d418793bc4da
Lewis Staples <lewis.staples@gmail.com>
2022-09-23 22:15:03 -0400

    Day 11 Rethinking approach ... using only a single data structure

--- day11/day11.py
+++ day11/day11.py
@@ -1,48 +1,57 @@
 # adventOfCode 2016 day 11
 # https://adventofcode.com/2016/day/11
 
-import csv
-import copy
-import itertools
+# import copy
+# import itertools
+import sys
 
-# Using below data as periodic_table.csv
-# https://gist.github.com/GoodmanSciences/c2dd862cd38f21b0ad36b8f96b4bf1ee
-with open('periodic_table.csv', mode='r') as element_file:
-    reader = csv.reader(element_file)
-    element_to_abbrev = {(rows[1].lower()):rows[2] for rows in reader if rows[1] != 'Element'}
 
-previous_states_set = set()
-current_states_list = list()
-current_states_set = set()
-future_states_list = list()
-future_states_set = set()
+# List of lists showing the current state
+current_state = [[], [], 1, 0]
 
-# Dict showing the current state
-current_state = dict()
+def invalid_state(state):
+    for i, chip_index in enumerate(state[0]):
+        gen_index = state[1][chip_index]
+        # If chip and generator are both on the same floor
+        if chip_index == gen_index:
+            # this particular chip is safe
+            continue
+        # Since this chip's generator is on another floor,
+        # if there are any generators on this floor
+        if chip_index in state[1]:
+            # This chip would get damaged by the generator, so the state is invalid
+            return True
 
-def invalid_combination(combination):
-    generator_types = set()
-    microchip_types = set()
-    for component in combination:
-        if component[-1] == 'G':
-            # generator_types.
-            generator_types.add(component[:-2])
-        elif component[-1] == 'M':
-            microchip_types.add(component[:-2])
-    
-    # If there are any microchips
-    if len(generator_types) > 0:
-        # and if there are any generators without a matching microchip
-        if len(microchip_types - generator_types) > 0:
-            return False
-    
-    return True
+    # No chips have generators that are a threat, thus the state is valid (hence, not invalid)
+    return False
+
+# This converts to tuples to lists.
+def make_state_mutable(state_immutable, ele_floor, step_count):
+    mutable_state = [list(state_immutable[0]), list(state_immutable[1]), ele_floor, step_count]
+    # for 
+    return mutable_state
+
+
+
+# This converts lists to tuples.  It also orders chips in ascending order and re-orders the generators to continue to match the chips
+def make_state_immutable(state):
+    new_state_0 = sorted(state[0])
+    new_state_1 = [x for _,x in sorted(zip(state[0],state[1]))]
+    return_state = (tuple(new_state_0), tuple(new_state_1))
+    return return_state, state[2], state[3]
 
 # Reading input from the input file
-input_filename='input_sample0.txt'
+input_filename='input.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     floor_num_lookup = {'first': 1, 'second': 2, 'third': 3, 'fourth': 4}
+    
+    # Used to store data coming from the text file, before re-processing it later
+    initial_state_gens = dict()
+    initial_state_chips = dict()
+    
+    for i in range(1,5):
+        initial_state_chips[i] = []
 
     # Pull in each line from the input file
     for in_string in f:
@@ -53,95 +62,67 @@ with open(input_filename) as f:
         assert(in_string[0:4] == 'The ')
         in_string = in_string[4:]
 
-        # Get floor number
+
+        # Get floor number from input file
         floor_num_str, floor_dummy, contains_dummy, in_string = in_string.split(' ', 3)
         floor_num = floor_num_lookup[floor_num_str]
         del floor_dummy, contains_dummy, floor_num_str
 
+        # Loop through all devices on this line in input file (they're all on the same floor)
         while True:
+            # Find the index of the next device
             i_chip = in_string.find('-compatible microchip')
             i_gen = in_string.find(' generator')
-
             if i_chip == i_gen == -1:
                 # both find commands returned -1, thus neither string was found
                 break
-
             if i_chip == -1:
                 i_right = i_gen
             elif i_gen == -1:
                 i_right = i_chip
             else:
                 i_right = min(i_gen, i_chip)
-            index_str = element_to_abbrev[in_string[2:i_right]] + '-' + ('M' if in_string[i_right] == '-' else 'G')
+            
+            index_str = in_string[2:i_right]
+            if in_string[i_right] == '-':
+                initial_state_chips[floor_num].append(index_str)
+            else:
+                initial_state_gens[index_str] = floor_num
             in_string = in_string[in_string.find('a ', 1):]
-            current_state[index_str] = floor_num
-current_state['Elev.'] = 1
-# del floor_num_lookup
+print()
 
-current_states_list.append(current_state)
-current_states_set.add(tuple(sorted(current_state.items())))
+del floor_num_lookup
+
+for floor_chip in range(1, 5):
+    for element in initial_state_chips[floor_chip]:
+        floor_gen = initial_state_gens[element]
+        current_state[0].append(floor_chip)
+        current_state[1].append(floor_gen)
+
+
+current_state_immutable, ele_floor, step_count = make_state_immutable(current_state)
+known_states = {current_state_immutable: (ele_floor, step_count)}
+
 
 while True:
-    # Traverse current_states_list
-    for current_state in current_states_list:
-        current_floor = current_state['Elev.']
-        current_floor_items = {item for item in current_state if current_state[item] == current_floor and item != 'Elev.'}
+    # current_state = next_states.pop(0):
+    current_state_immutable, ele_floor, step_count = make_state_immutable(current_state)
+    if current_state_immutable not in known_states:
+        # check if this is the answer ... if yes ... output the solution
+        if set(current_state[0]) == {4} and set(current_state[1]) == {4}:
+            print(f'The answer to A has been found:  {step_count} steps are required')
+            sys.exit('Program exiting successfully')
+        known_states[current_state_immutable] = (ele_floor, step_count)
 
-        # Consider new floors for elevator
-        for floor_delta in [-1,1]:
-            new_floor = current_floor + floor_delta
-            if new_floor in floor_num_lookup.values():
-                new_state = copy.deepcopy(current_state)
-                new_state['Elev.'] += floor_delta
-                future_states_list.append(new_state)
-        
-        # look for anything else on current state's floor that could be brought to new_state's floor
-        choices_to_move = list(itertools.combinations(current_floor_items, 1)) + list(itertools.combinations(current_floor_items, 2))
-        for choice_move in choices_to_move:
-            # Determine what choice_move will leave behind
-            left_behind = current_floor_items - set(choice_move)
-            if invalid_combination(left_behind):
-                continue
-            for new_state in future_states_list:
-                new_floor = new_state['Elev.']
-                newest_state = copy.deepcopy(new_state)
-                for item_to_move in choice_move:
-                    newest_state[item_to_move] = new_floor
-                    if invalid_combination([item for item, floor in newest_state.items() if floor == new_floor]):
-                        continue
-                    # future_states_list.append(newest_state)
-                    newest_state_tuple = tuple(sorted(newest_state.items()))
-                    if newest_state_tuple not in previous_states_set and newest_state_tuple not in current_states_set:
-                        future_states_set.add(newest_state_tuple)
-    
-    previous_states_set.update(current_states_set)
-    current_states_set.clear()
-    current_states_set.update(future_states_set)
-    future_states_set.clear()
-    current_states_list.clear()
+        current_state = make_state_mutable(current_state_immutable, ele_floor, step_count)
+        # determine adjacent states, and add valid states to next_states
+    break
 
-    for current_state_tuple in current_states_set:
-        current_state_dict = dict()
-        for key, value in current_state_tuple:
-            current_state_dict[key] = value
-        
-        if set(current_state_dict.values()) == {4}:
-            break
+print('Program has ended without finding the answer\n')
 
-        current_states_list.append(current_state_dict)
-    future_states_list = list()
+# current_states_list.append(current_state)
+# current_states_set.add(convert_state_to_hashable(current_state))
 
-
-
-        # For each current_state, come up with future_states
-            # degrees of freedom:
-            # Elevator can only go up or down by one floor
-            # Elevator must carry either 1 or 2 items
-            # constraints
-            # Elevator cannot go outside of floors [1,4]
-            # You can't have a generator along with a non-matching chip on any floor, including elevator
-
-        # Verify that that future state isn't already in any set
 
 
 
