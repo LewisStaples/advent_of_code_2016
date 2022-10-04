d62b138ef37f3afec59b594ac6244dc7adcc335c
Lewis Staples <lewis.staples@gmail.com>
2022-10-04 18:04:22 -0400

    Day 17 Part B complete

---
+++ day17/day17_partB.py
@@ -0,0 +1,49 @@
+# adventOfCode 2016 day 17
+# https://adventofcode.com/2016/day/17
+
+
+import hashlib
+
+
+# Reading input from the input file
+input_filename='input_sample1.txt'
+with open(input_filename) as f:
+    passcode_str = f.readline().rstrip()
+print(f'\nUsing input file: {input_filename}')
+print(f'and this file contains {passcode_str}')   
+ 
+# Collection that lists all points on the board:
+permissible_points = set()
+for i in range(4):
+    for j in range(4):
+        permissible_points.add((i,j))
+
+current_paths = {'':(0,0)}
+destination = (3,3)
+longest_path_len = 0
+next_paths = {}
+directions = {'U':(0,-1), 'D':(0,1), 'L':(-1,0), 'R':(1,0)}
+while True:
+    for path, point in current_paths.items():
+        hash_str = hashlib.md5((passcode_str + path).encode()).hexdigest()
+        for i, (direction, step) in enumerate(directions.items()):
+            # check if next_point is outside the grid
+            next_point = (point[0] + step[0], point[1] + step[1])
+            if next_point not in permissible_points:
+                continue
+            # check if the door is open in this direction
+            if hash_str[i] not in ['b', 'c', 'd', 'e', 'f']:
+                continue
+            # both checks above confirm that next_point can be visited next
+            new_path = path + direction
+            if next_point == destination:
+                longest_path_len = len(new_path)
+                continue
+            next_paths[new_path] = next_point
+    if len(next_paths) == 0:
+        break
+    current_paths = next_paths
+    next_paths = {}
+
+print(f'The length of the longest path is: {longest_path_len}\n')
+
