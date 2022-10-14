18b33efa2b1d873ec34048889d650dcf6ddb0e99
Lewis Staples <lewis.staples@gmail.com>
2022-10-13 21:19:58 -0400

    Day 22 Part A working

--- day22/day22_partA.py
+++ day22/day22_partA.py
@@ -2,6 +2,18 @@
 # https://adventofcode.com/2016/day/22
 
 
+from collections import namedtuple
+
+node_list = []
+count_valid_pairs = 0
+Node = namedtuple('Node', ['Filesystem', 'Size', 'Used', 'Avail', 'UsePct'])
+
+def remove_unit(in_string):
+    if not in_string.isdigit():
+        if in_string[:-1].isdigit():
+            return int(in_string[:-1])
+    return in_string
+
 # Reading input from the input file
 input_filename='input.txt'
 print(f'\nUsing input file: {input_filename}\n')
@@ -11,11 +23,19 @@ with open(input_filename) as f:
         if line_num < 2:
             continue
         in_string = in_string.rstrip()
-        print(in_string)
         in_fields = in_string.split()
-        # print(in_string.split())
-        print()
+        in_fields = [remove_unit(x) for x in in_fields]
+        node_list.append(Node(*in_fields))
 
+for i, nodeA in enumerate(node_list):
+    if nodeA.Used == 0:
+        continue
+    for j, nodeB in enumerate(node_list):
+        if i == j:
+            continue
+        if nodeA.Used <= nodeB.Avail:
+            count_valid_pairs += 1
 
+print(f'The answer to part A is {count_valid_pairs}\n')
 
 
